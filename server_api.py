583219c848e81ebf23301ada58fb00458f1d45ff
jayesh-bhai <unknownjayesh05@gmail.com>
2025-03-31 23:38:43 +0530

    Entire project is now connected

--- server_api.py
+++ server_api.py
@@ -1,19 +1,13 @@
 from flask_cors import CORS
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
-import pickle
-import os
 
-app = Flask(__name__, static_folder="build", static_url_path="/")
+app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # ✅ Load the trained model
 model = load_model("ml-model/Trained-Model/global_model_sahiel.h5")
-
-@app.route("/")
-def serve():
-    return send_from_directory(app.static_folder, "index.html")
 
 
 @app.route("/predict", methods=["POST"])
@@ -42,5 +36,5 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
 
